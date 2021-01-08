cd2626a71d3ba39f33f15a1e7804174c6e1231b2
DM <ilostinyoureyes@gmail.com>
2021-01-08 17:41:24 +0800

    chore: update lib entry file.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
-// import { loadConfigFromEnv, replaceHomeDir } from './utils'
-
-// const config = loadConfigFromEnv()
-// if (!config) {
-//   throw new Error('get config failed')
-// }
-
-// const server = {
-//   stg: {
-//     host: config.STG_HOST,
-//     port: config.STG_PORT,
-//     user: config.STG_USER,
-//     privateKey: replaceHomeDir(config.STG_PRIVATE_KEY),
-//   },
-// }
-
-// console.log(server)
-
-// // replaceHomeDir('~/.ssh/authorized_keys/../../.ssh/id_rsa/~/asd')
-
-import { getEnvData } from './utils'
-getEnvData()
+export { runRemoteShell } from './utils/runRemoteShell'
+export { loadConfigFromEnv, DotenvParseOutput } from './utils/loadConfigFromEnv'
+export { replaceHomeDir } from './utils/replaceHomeDir'
+export { getEnvData } from './utils/getEnvData'
+export { packageFolder } from './utils/packageFolder'
+export { generateIncrementalPackage } from './utils/generateIncrementalPackage'
--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export { runRemoteShell } from './runRemoteShell'
-export { loadConfigFromEnv, DotenvParseOutput } from './loadConfigFromEnv'
-export { replaceHomeDir } from './replaceHomeDir'
-export { getEnvData } from './getEnvData'
-export { packageFolder } from './packageFolder'
-export { generateIncrementalPackage } from './generateIncrementalPackage'
