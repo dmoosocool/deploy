cd9715e369630ff47e6072deb03efbb2c61f714d
DM <ilostinyoureyes@gmail.com>
2021-01-21 20:15:22 +0800

    fix: fixed generateIncrementalPackage

--- test/generateIncrementalPackage.ts
+++ test/generateIncrementalPackage.ts
@@ -1,12 +1,16 @@
 import { generateIncrementalPackage } from '../'
+import * as fs from 'fs'
 const incrementalPackagePath = generateIncrementalPackage(
-  '~/.sjkh/uploads/stg/full/20210121154606778.zip',
-  '~/.sjkh/uploads/stg/full/20210121152931488.zip',
+  '~/.sjkh/uploads/stg/full/20210121201244206.zip',
+  '~/.sjkh/uploads/stg/full/20210121200728170.zip',
   '~/.sjkh/uploads/stg/incremental/'
 )
 
 if (incrementalPackagePath !== '') {
   console.log(incrementalPackagePath)
+  if (!fs.existsSync(incrementalPackagePath)) {
+    console.log('生成的增量包路径不对')
+  }
 } else {
   console.log('没有变更的文件')
 }
