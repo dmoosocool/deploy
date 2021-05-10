3c7b36addcfc1828f6c13faf843478e1424994aa
DM <dmoosocool@gmail.com>
2021-05-10 11:26:55 +0800

    chore: delete lib folder, upgrade dependencies.

--- lib/utils/generateIncrementalPackage.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 根据两个zip包, 生成增量包
- *
- * @param {string} newzip 较新的zip资源包路径
- * @param {string} oldzip 较旧的zip资源包路径
- * @param {string} incrementalPath 生成的增量包所在的路径
- */
-export declare function generateIncrementalPackage(newzip: string, oldzip: string, incrementalPath: string): string;
--- lib/utils/getEnvData.d.ts
+++
@@ -1,5 +0,0 @@
-import { DotenvParseOutput } from '../';
-/**
- * 获取env文件数据
- */
-export declare function getDataByEnv(env: string): DotenvParseOutput;
