c190e53968c4c0b2fd6ad478ff7ae0923360a90f
dmoosocool <dmoosocool@gmail.com>
2021-05-11 00:03:08 +0800

    test: add getDataByEnv unit test.

--- test/test-lib/getDataByEnv.test.ts
+++ test/test-lib/getDataByEnv.test.ts
@@ -3,7 +3,7 @@ import * as chai from 'chai'
 const expect = chai.expect
 
 describe('@dmoosocool/deploy `getDataByEnv`', () => {
-  it(`get default config datas by empty env string`, () => {
+  it('get default config datas by empty env string', () => {
     const defaultEnvDatas = getDataByEnv()
     expect(defaultEnvDatas).to.contains.keys(
       '_env',
@@ -16,5 +16,20 @@ describe('@dmoosocool/deploy `getDataByEnv`', () => {
     )
 
     expect(defaultEnvDatas._env).to.equal('stg')
+  })
+
+  it('get stg config datas by "stg" env string', () => {
+    const stgEnvDatas = getDataByEnv('stg')
+    expect(stgEnvDatas).to.contains.keys(
+      '_env',
+      '_supportedEnv',
+      'host',
+      'port',
+      'username',
+      'password',
+      'private_key'
+    )
+
+    expect(stgEnvDatas._env).to.equal('stg')
   })
 })
